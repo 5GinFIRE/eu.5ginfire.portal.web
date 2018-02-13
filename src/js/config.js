ba2f66d3b5cc2f4e0d66b6b8691b4ddf1284d1ea
Christos Tranoris <tranoris@gmail.com>
2018-02-13 13:31:21 +0200

    wiki link updated

--- src/js/config.js
+++ src/js/config.js
@@ -3,9 +3,10 @@ var appConfig = angular.module('portalwebapp.config',[]);
 
 appConfig.factory('APIEndPointService', function() {
 	  return {	      
+
 		  //APIURL: "/portal/"
-		  APIURL: "http://localhost:13000/5ginfireportal/", //good to test CROSS ORIGIN scenarios. use with http://127.0.0.1/mp
-		  WEBURL: "http://localhost:13000/mp"
+		  APIURL: "http://150.140.184.212:13000/5ginfireportal/", //good to test CROSS ORIGIN scenarios. use with http://127.0.0.1/mp
+		  WEBURL: "http://localhost:13000/mp/"
 		  //APIURL: "http://localhost:13000/5ginfireportal/" //good to test CROSS ORIGIN scenarios. use with http://127.0.0.1/mp
 		  //APIURL: "http://83.212.106.218:8080/5ginfireportal/"
 	  };
