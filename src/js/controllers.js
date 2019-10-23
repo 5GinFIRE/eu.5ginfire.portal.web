0d7b69e419ddc10463170919ae6488839de39fa5
Christos Tranoris <tranoris@gmail.com>
2019-10-23 10:25:24 +0300

    resend

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -25,8 +25,8 @@ appControllers.controller('FeaturedApps', ['$scope','$window','$log', 'Experimen
 }]);
          	
          	
-appControllers.controller('UserListController', ['$scope','$window','$log', 'PortalUser', 'popupService', 'ngDialog', 'APIEndPointService',
-                            	function($scope, $window, $log, PortalUser, popupService, ngDialog, APIEndPointService) {
+appControllers.controller('UserListController', ['$scope','$window','$log', 'PortalUser', 'popupService', 'ngDialog', 'APIEndPointService', '$http',
+                            	function($scope, $window, $log, PortalUser, popupService, ngDialog, APIEndPointService, $http) {
 	
 	
 
